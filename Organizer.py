2a7c3df460b886ea797bb3672d24899ca6ddbfab
Rethakgetse-Manaka <rethakgetse11@gmail.com>
2024-07-16 19:18:56 +0200

    "Added on modified, on cretaed and on moved. This helps with CRUD actions."

--- Organizer.py
+++ Organizer.py
@@ -5,7 +5,19 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 class FileOrganizerHandler(FileSystemEventHandler):
+    def __init__(self):
+        self.organize_files()
+
+    def on_moved(self, event):
+        self.organize_files()
+        
+    def on_created(self, event):
+        self.organize_files()
+
     def on_modified(self, event):
+        self.organize_files()
+
+    def organize_files(self):
         for filename in os.listdir(folder_to_track):
             src = os.path.join(folder_to_track, filename)
             if os.path.isfile(src):
@@ -25,12 +37,22 @@ class FileOrganizerHandler(FileSystemEventHandler):
 folder_to_track = os.path.expanduser('~/Downloads')  # This sets the Downloads folder for the current user
 extension_to_folder = {
     'jpg': 'Images', 'jpeg': 'Images', 'png': 'Images', 'gif': 'Images',
-    'pdf': 'Documents', 'docx': 'Documents', 'xlsx': 'Documents', 'txt': 'Documents',
+    'pdf': 'Documents', 'docx': 'Documents', 'xlsx': 'Documents', 'txt': 'Documents', 'csv' : 'Documents', 
+    'pptx' : 'Documents', 'doc' : 'Documents', 'xls' : 'Documents', 'ppt' : 'Documents', 
+    'txt' : 'Documents',
     'mp4': 'Videos', 'mkv': 'Videos', 'mov': 'Videos', 'avi': 'Videos',
     'mp3': 'Music', 'wav': 'Music', 'flac': 'Music', 'm4a': 'Music',
     'zip': 'Compressed', 'rar': 'Compressed', '7z': 'Compressed', 'tar': 'Compressed',
     'exe': 'Programs', 'msi': 'Programs', 'deb': 'Programs', 'pkg': 'Programs',
-    '': 'Others'
+    '': 'Others',
+    'java' : 'Java-Programs', 'py':'Python-Programs', 'c':'C-Programs', 'cpp':'C++-Programs', 'html':'HTML-Programs', 
+    'css':'CSS-Programs', 'js':'JS-Programs', 'php':'PHP-Programs', 'sql':'SQL-Programs', 'sh':'Shell-Programs', 
+    'rb':'Ruby-Programs', 'pl':'Perl-Programs', 'swift':'Swift-Programs', 'kt':'Kotlin-Programs', 'go':'Go-Programs', 
+    'ts':'TypeScript-Programs', 'r':'R-Programs', 'cs':'C#-Programs', 'vb':'VB-Programs', 'scala':'Scala-Programs', 
+    'rust':'Rust-Programs', 'dart':'Dart-Programs', 'lua':'Lua-Programs', 'ts':'TypeScript-Programs', 'asm':'Assembly-Programs', 
+    'json':'JSON-Programs', 'xml':'XML-Programs', 'yml':'YAML-Programs', 'toml':'TOML-Programs', 'yaml':'YAML-Programs', 
+    'ini':'INI-Programs', 'cfg':'CFG-Programs', 'conf':'CONF-Programs', 'log':'LOG-Programs', 'md':'Markdown-Programs', 'tex':'TEX-Programs' 
+
 }
 
 event_handler = FileOrganizerHandler()
